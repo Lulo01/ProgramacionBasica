6ab0a11fdd25250964a20c4034eebd5a88aa6ee2
Lulo <luciana.gomez0110234@gmail.com>
2025-09-25 22:37:09 -0500

    Add files via upload

---
+++ Corte1C++/Practica10/main.cpp
@@ -0,0 +1,11 @@
+// Ejemplo de un loop infinito en C++ usando "for loop"
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Infinite loop using while
+	while (true) {
+		cout << "Esto es un loop infinito." << endl;
+	}
+	return 0;
+}
