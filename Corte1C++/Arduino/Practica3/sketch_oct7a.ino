be6d8fb10fccfecd315a622fc1eb6937f28aec2f
Lulo <luciana.gomez0110234@gmail.com>
2025-10-07 15:27:34 -0500

    Add files via upload

---
+++ Corte1C++/Arduino/Practica3/sketch_oct7a.ino
@@ -0,0 +1,18 @@
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+
+  int blinkCount = 0;
+
+  while (blinkCount < 5) {
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    delay(500);
+    blinkCount++;
+  }
+}
