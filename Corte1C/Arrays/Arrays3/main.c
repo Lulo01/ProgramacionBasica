304c4fd720a3de5db88d545f7a447811cbeaab00
Lulo <luciana.gomez0110234@gmail.com>
2025-10-21 15:30:09 -0500

    Add files via upload

---
+++ Corte1C/Arrays/Arrays3/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int myNumbers[4];
+	
+	myNumbers[0] = 65;
+	myNumbers[1] = 20;
+	myNumbers[2] = 7;
+	myNumbers[3] = 10;
+	
+	printf ("%d\n", myNumbers[1]);
+	
+	return 0;
+
+}
