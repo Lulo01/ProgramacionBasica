3817db9d00c75495ca72a660960eb81579f441ac
Lulo <luciana.gomez0110234@gmail.com>
2025-10-21 15:40:14 -0500

    Add files via upload

---
+++ Corte1C/Arrays/Arrays4/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int numeros[] = {25, 50, 70, 3.15, 5.99};
+	int i;
+	
+	for (i = 0; i < 5; i++) {
+		printf ("%d\n", numeros[i]);
+	}
+	
+	return 0;
+}
+
